818560de95b9187f52a810c3456c4a15b26cb771
aramzi296 <ahmad.ramzi@gmail.com>
2026-05-22 08:13:38 +0700

    db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             DistrictSubdistrictSeeder::class,
-            CategoriesSeeder::class,
+            CategorySubcategorySeeder::class,
             
         ]);
     }
